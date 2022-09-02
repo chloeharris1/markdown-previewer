c5267f8e6eb67836e47927c6c51eb0110683f679
Chloe Harris <chloe.a.harris17@gmail.com>
2022-09-02 13:25:18 -0700

    Added markdown placeholder text, initial page layout

--- README.md
+++ README.md
@@ -4,32 +4,42 @@
 
 
 ## Description
+----------------
+A React Markdown Previewer app which contains a textarea for Markdown input and a preview tab where the converted text will appear.
 
+A FreeCodeCamp Front End Development Libraries project, with the objective to fulfill the user stories and get all of the tests to pass.
 
-## Built With 
+## Built With
+----------------
 
 ![image](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)
 
 ![image](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
 
-### Dependencies 
-* Marked - Markdown Parser 
+## Dependencies 
+----------------
+**Marked** - Markdown Parser 
 
 ## Snapshot 
+----------------
 
 
 ## Link 
+----------------
 
 
 ## Credits
+----------------
 
 Favicon from: 
 [freepik](https://www.freepik.com/)
 
 ## License
+----------------
 Licensed under the MIT license.
 
 ## Questions 💬
+----------------
 [Email Me](Chloe.a.harris17@gmail.com) if you have any questions.
 
 Check out more of my work on [GitHub](https://github.com/chloeharris1).
--- src/App.js
+++ src/App.js
@@ -1,21 +1,89 @@
 import React from 'react'
 import './styles.css';
+// Import Marked library
+// import marked from 'marked';
+
+const placeholder = `#Heading (H1)
+## Sub-heading (H2)
+### Another deeper heading (H3)
+
+Text Attributes:
+**bold text**
+*italicized text*
+~~strikethrough~~
+> blockquote
+
+Ordered List:
+1. First Item
+2. Second Item
+3. Third Item
+
+Unordered List:
+- First Item
+- Second Item
+- Third Item
+
+Code:
+\`<div>Hello World</div>\`
+
+Multi-line Code Block:
+\`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+
+Horizontal Rule
+___
+
+Link: [title](https://www.example.com)
+
+Image: ![alt text](image.jpg)
+
+Table: 
+| Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text |
+
+Task List:
+- [x] Write the press release
+- [ ] Update the website
+- [ ] Contact the media
+
+Highlight: I need to highlight these ==very important words==.
+
+Emoji: That is so funny! :joy:
+
+`;
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    // Initialize state
+    this.state = {
+      markdown: placeholder,
+    };
+  }
   render(){
     return (
-      <div>
-        <h1>Markdown Previewer</h1>
-        <textarea id="editor">
+      <div className='container'>
+        <div className='row d-flex justify-content-center text-center'>
+          <h1>Markdown Previewer</h1>
+          <textarea id="editor" className='col-md-6 col-xs-12' value={this.state.markdown}>
 
-        </textarea>
-        <div id="preview">
+          </textarea>
+          <div id="preview" className='col-md-6 col-xs-12'>
 
+          </div>
         </div>
-
       </div>
     );
   }
 }
+
+
 
 export default App;
